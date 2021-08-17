d6d00103fc34e6885218b9e0fdc7660d5e988ea4
Rajarajan Kumanan <rajarajankumanan@gmail.com>
2021-08-17 11:37:21 +0530

    section drag and drop completed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { CdkDragDrop, moveItemInArray, transferArrayItem } from '@angular/cdk/drag-drop';
 import { Component, ViewEncapsulation } from '@angular/core';
 
 @Component({
@@ -15,4 +16,22 @@ import { Component, ViewEncapsulation } from '@angular/core';
   encapsulation: ViewEncapsulation.None
 })
 export class AppComponent {
+
+  public section_list: any = {
+    section_id: ["c1", "t1", "i1", "h1", "o1", "o2"],
+    section_arr: ["Candidates_List", "Technical_Test", "Interview", "HR", "Offer_Accepted", "Offer_Rejected"]
+  }
+
+  timePeriods = [
+    'Bronze age',
+    'Iron age',
+    'Middle ages',
+    'Early modern period',
+    'Long nineteenth century'
+  ];
+
+  drop(event: CdkDragDrop<string[]>) {
+    moveItemInArray(this.section_list.section_arr, event.previousIndex, event.currentIndex);
+  }
+
 }
